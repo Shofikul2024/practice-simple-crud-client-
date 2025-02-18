70d71e08f5737147accab766049e080f6813eee0
Shofikul2024 <islamshofi942@gmail.com>
2025-02-18 14:45:10 +0600

    arrow fun add  commit

---
+++ arrow.js
@@ -0,0 +1,33 @@
+
+//  using arrow function   division  two number . 
+
+// const division =( num1 ,num2)=> num1/num2
+// const result=division( 100 ,5)
+// console.log(result)
+
+
+
+
+//  using arrow function  subtraction  two number . 
+
+
+
+// const subtract= ( num1 ,num2) => num1 -num2
+// const result = subtract ( 20,10)
+// console.log(result)
+
+
+
+
+//  using arrow function  multiply two number . 
+
+
+
+// const multiply= ( num1 ,num2) => num1 *num2
+// const result = multiply( 20,10)
+// console.log(result)
+
+
+const add =(num1 ,num2) => num1 +num2
+const result = add ( 20 ,30)
+console.log( result);
